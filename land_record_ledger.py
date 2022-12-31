eaeedf5c039bcb02438720e4cb15476c0fdf9f08
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2022-12-31 16:01:10 +0530

    Proof of work and hash function added

--- land_record_ledger.py
+++ land_record_ledger.py
@@ -15,12 +15,36 @@ class Blockchain:
     def create_block(self, owner, Reg_no, proof, previous_hash):
         #creating Dictionary i.e, block
         block = {
-            'owner':owner,
-            'Reg_no': Reg_no,
-            'index': len(self.chain)+1,
-            'timestamp': str(datetime.datetime.now),
-            'proof': proof,
-            'previous_hash': previous_hash
+            'owner':owner,                          #constant
+            'Reg_no': Reg_no,                       #constant
+            'index': len(self.chain)+1,             #constant
+            'timestamp': str(datetime.datetime.now),#constant
+            'proof': proof,                         #Keep changing
+            'previous_hash': previous_hash          #constant
         }
         self.chain.append(block)
         return block
+
+    # Proof of work :  We want a fixed number of zeros in our hash to be in our blockchain
+    # Eg - 0000shdhwbd#$#%$fdj  So we have 4 zero's in starting of our hash
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while check_proof is False:
+            # **2 means square
+            # encode() always works on String
+            hash_val = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_val[:4] == '0000':
+                check_proof = True
+            else :
+                new_proof += 1
+
+        return new_proof
+    
+    # It will take a block and produces a Hash value of it
+    def hash(self, block):
+        # Block is a dic , dump() convert dic to String, as encode works on str only
+        encoded_block = json.dumps(block).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
